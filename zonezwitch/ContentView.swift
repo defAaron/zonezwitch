31cb8bd0520fa012a7ca11c33e068fd2b94cb9da
Aaron Dutta <aaronsumit123@gmail.com>
2025-11-26 21:49:26 -0500

    Initial Commit

---
+++ zonezwitch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  zonezwitch
+//
+//  Created by Aaron Dutta on 2025-11-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ zonezwitch/zonezwitchApp.swift
@@ -0,0 +1,17 @@
+//
+//  zonezwitchApp.swift
+//  zonezwitch
+//
+//  Created by Aaron Dutta on 2025-11-26.
+//
+
+import SwiftUI
+
+@main
+struct zonezwitchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
